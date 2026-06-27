e959552843a483d1d0c1faf42b4171fca2d65951
dbruno2 <dbruno2@studenti.uninsubria.it>
2026-06-27 19:46:26 +0200

    impostazione generale serverTK.java con accept e ServerSocket; creazione della classe ServerSlave.java per gestire più client. Aggiunta di err.print piu robuste.

--- src/serverTK/ServerSlave.java
+++ src/serverTK/ServerSlave.java
@@ -1,4 +1,33 @@
 package serverTK;
 
-public class ServerSlave {
+import dao.GestioneTheKnife;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ServerSlave extends Thread {
+
+    Socket socket;
+    GestioneTheKnife gk;
+    ObjectInputStream in;
+    ObjectOutputStream out;
+
+    public ServerSlave(Socket socket, GestioneTheKnife gk) {
+        this.socket = socket;
+        this.gk = gk;
+        try {
+            in = new ObjectInputStream(socket.getInputStream());
+            out = new ObjectOutputStream(socket.getOutputStream());
+        } catch (Exception e) {
+            System.err.println("[ServerSlave] Impossibile aprire gli stream di I/O con il client "
+                    + socket.getRemoteSocketAddress()
+                    + " - " + e.getClass().getSimpleName() + ": " + e.getMessage());
+            e.printStackTrace(System.err);
+        }
+    }
+
+    public void run() {
+
+    }
 }
--- src/serverTK/serverTK.java
+++ src/serverTK/serverTK.java
@@ -1,22 +1,47 @@
 package serverTK;
 
-import dao.PostgresDB;
+import dao.GestioneTheKnife;
 
 import java.net.ServerSocket;
-import java.util.List;
-import java.util.Map;
+import java.net.Socket;
 
 public class serverTK {
 
     ServerSocket serverSocket;
-    int porta = 4444;
+    GestioneTheKnife gk;
+    public static final int PORTA = 4444;
 
+    public serverTK() {
+        try {
+            serverSocket = new ServerSocket();
+        } catch (Exception e) {
+            System.err.println("[serverTK] Impossibile creare il ServerSocket sulla porta " + PORTA
+                    + " - " + e.getClass().getSimpleName() + ": " + e.getMessage());
+            e.printStackTrace(System.err);
+        }
+    }
 
+    public void exc(){
+        System.out.println("server in esecuzione");
+        Socket socket;
+        while(true){
+            System.out.println("server in attesa di connessione");
+            try {
+               socket= serverSocket.accept();
+               new ServerSlave(socket,gk);
+            } catch (Exception e) {
+                System.err.println("[serverTK] Errore durante l'attesa/accettazione di una connessione client"
+                        + " - " + e.getClass().getSimpleName() + ": " + e.getMessage());
+                e.printStackTrace(System.err);
+            }
+        }
+    }
     /**
      * giusto per farci 2 prove della classe che interagisce con db, questo sarebbe il server nell'architettura master slave in realtà, giusto?
      */
     public static void main(String[] args) {
 
+        new serverTK().exc();
 
     }
 }
